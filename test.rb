a0a8726ec2c9f66c9b1005b53b11c531e255460f
Blake Strickland <Stricklandb02@gmail.com>
2015-09-21 21:36:28 -0400

    Pass test weather alert

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './current_observation'
 require './sun_rise_set.rb'
 require './10_day_forecast'
+require './weather_alert'
 require 'json'
 
 # class CurrentObservation
@@ -17,12 +18,15 @@ class CurrentTest < Minitest::Test
   def test_current_temp
     assert_equal 79.1, CurrentObservation.new(27613).current_temp
   end
+
   def test_current_wind
     assert_equal 5.0, CurrentObservation.new(27613).current_wind
   end
+
   def test_current_weather
     assert_equal "Overcast", CurrentObservation.new(27613).current_weather
   end
+
   def test_current_visability
     assert_equal "10.0", CurrentObservation.new(27613).current_visability_mi
   end
@@ -42,11 +46,15 @@ class CurrentTest < Minitest::Test
       assert_equal a, TenDayForecast.new(27613).ten_day_conditions
   end
 
-
   def test_sunrise_and_sunset
      assert_equal "7:01 a.m.", SunRiseSet.new(27613).sun_rise
      assert_equal "19:13 p.m.", SunRiseSet.new(27613).sun_set
    end
+
+  def test_weather_alert
+    a = ["Areal Flood Advisory, Flash Flood Watch"]
+    assert_equal a,WeatherAlert.new(27613).weather_alerts
+  end
 
 
 end
