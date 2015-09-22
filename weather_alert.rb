790ba6a13f4a1e6ed3b6351ba816baa5cdb5cb42
Blake Strickland <Stricklandb02@gmail.com>
2015-09-21 20:26:22 -0400

    list all weather alerts

--- weather_alert.rb
+++ weather_alert.rb
@@ -5,9 +5,24 @@ class WeatherAlert
     @zip_code = zip_code
     @response = get_response
   end
+
+
+  # def weather_alerts
+  #   @response["alerts"][0]["description"]
+  # end
+
+
   def weather_alerts
-    @response["alerts"][0]["description"]
+    # binding.pry
+    simplified = []
+    @response["alerts"].each do |x|
+      #my_pretty = x["stormName_Nice"].to_s
+      simplified << x["description"]
+    end
+    simplified
   end
+
+
   private def get_response
   file = File.read('weather_alert.json')
     data_hash = JSON.parse(file)
